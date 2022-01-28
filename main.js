1944b1dd1e30e0c5a647269339dbb14f455e50a2
TaekJin Jang <xorwls1500@naver.com>
2022-01-29 00:37:57 +0900

    -

--- main.js
+++ main.js
@@ -12,3 +12,18 @@ const getLatesNews = async () => {
 };
 
 getLatesNews();
+
+const openSearchBox= () =>{
+    let inputArea = document.getElementById("input-area");
+    if (inputArea.style.display==="inline") {
+        inputArea.style.display= "none";
+    }else {
+        inputArea.style.display = "inline"
+    }
+};
+const openNav = () => {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+const closeNav = () => {
+    document.getElementById("mySidenav").style.width = "0";
+}
