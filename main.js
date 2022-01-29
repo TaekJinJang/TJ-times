980467c29f85bf55accc0bc140e7bc6e06c66552
TaekJin Jang <xorwls1500@naver.com>
2022-01-29 10:57:55 +0900

    -

--- main.js
+++ main.js
@@ -1,59 +1,64 @@
-let news = []
+let news = [];
+
 
 const getLatesNews = async () => {
-    let url = new URL("https://api.newscatcherapi.com/v2/search?q=Tesla&page_size=2");
-    let header = new Headers({'x-api-key' : 'RXXn8l8Gh869EmgoqqCdGYYZf4yaZujLFvBlFgeaTt0'})
+  let url = new URL(
+    "https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=food&page_size=10"
+  );
+  let header = new Headers({
+    "x-api-key": "RXXn8l8Gh869EmgoqqCdGYYZf4yaZujLFvBlFgeaTt0",
+  });
 
-    let response = await fetch(url,{headers:header}) // 데이터보내는방식은 ajax, http, fetch 등이 있음
-    let data = await response.json()
-    console.log(data);
-    news = data.page
-    console.log(articles);
+  let response = await fetch(url, { headers: header }); // 데이터보내는방식은 ajax, http, fetch 등이 있음
+  let data = await response.json();
+  console.log(data);
+  news = data.articles;
+  console.log(news);
+  render();
 };
 
 getLatesNews();
-
-const openSearchBox= () =>{
-    let inputArea = document.getElementById("input-area");
-    if (inputArea.style.display==="inline") {
-        inputArea.style.display= "none";
-    }else {
-        inputArea.style.display = "inline"
-    }
-};
-const openNav = () => {
-    document.getElementById("mySidenav").style.width = "250px";
-}
-const closeNav = () => {
-    document.getElementById("mySidenav").style.width = "0";
-}
-let taskList=[]
-function addTask() {
-    let task = {
-      id: randomIDGenerate(),
-      taskContent: taskInput.value, // api 불러옴
-    };
-    taskList.push(task);
-    render();
-  }
-  function randomIDGenerate() {
-    return "_" + Math.random().toString(36).substr(2, 9);
-  }
-
-  function render(){
-    let resultHTML = "";
-    for (let i = 0; i < list.length; i++) {
-        resultHTML += `<div class="row news">
+const render= () => {
+    let newsHTML = "";
+    newsHTML = news
+    .map((item) => {
+        
+        return `<div class="row news">
         <div class="col-lg-4">
-            <img class="news-img-size" src="https://static01.nyt.com/images/2022/01/27/world/27ukraine-biden-europe01SUB/merlin_200959887_762ebb63-110b-4da7-9f62-10675c5d4169-threeByTwoSmallAt2X.jpg?format=pjpg&quality=75&auto=webp&disable=upscale" alt="">
+        <img class="news-img-size" src="${item.media || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU"
+        }"  />
         </div>
         <div class="col-lg-8">
-            <h2>졸려주ㅡㄱ겠다</h2>
-            <p>대충끝내고 눵야지</p>
-            <div>
-               유튜브봐야함20220128
-            </div>
+        <h2>${item.title}</h2>
+        <p>${
+            item.summary == null || item.summary == ""
+            ? "내용없음"
+            : item.summary.length > 200
+            ? item.summary.substr(0,200) + "..."
+            : item.summary
+        }</p>
+        <div>${item.rights || "no source"} ${moment(
+            item.published_date
+         ).fromNow()}</div>
         </div>
-    </div>`;
-    }   
+        </div>`;
+    })
+    .join(" ");
+    console.log(newsHTML);
+    document.getElementById("news-board").innerHTML = newsHTML;
+}
+
+const openSearchBox = () => {
+  let inputArea = document.getElementById("input-area");
+  if (inputArea.style.display === "inline") {
+    inputArea.style.display = "none";
+  } else {
+    inputArea.style.display = "inline";
   }
+};
+const openNav = () => {
+  document.getElementById("mySidenav").style.width = "250px";
+};
+const closeNav = () => {
+  document.getElementById("mySidenav").style.width = "0";
+};
